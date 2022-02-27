681e6ed9ee4709f1d97411280e3d4db4b6dea3c6
Tofu <osawa.shuto@gmail.com>
2022-02-27 21:33:52 +0900

    ex6

---
+++ HeadFirstEx6/HeadFirstEx6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HeadFirstEx6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface Command
+    {
+        public void execute();
+    }
+
+    public class LightOnCommand: Command
+    {
+        Light light;
+        public LightOnCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void execute()
+        {
+            light.on();
+        }
+    }
+
+    public class Light
+    {
+        public void on()
+        {
+
+        }
+
+        public void off()
+        {
+
+        }
+    }
+}
